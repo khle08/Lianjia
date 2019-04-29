ba8ba285e8d451acdb986082fe4eec804b018a30
yawuplus <1397991131@qq.com>
2019-04-29 22:01:21 +0800

    a

--- dao/mongo.py
+++ dao/mongo.py
@@ -1,83 +1,32 @@
 import pymongo
+import time
 
 
 class mongo_dao():
-    def __init__(self, db):
+    def __init__(self):
         self.client = pymongo.MongoClient()
+        db = 'lianjia_newhouse' + str(time.strftime('%Y%m%d', time.localtime(time.time())))
         self.db = self.client[db]
         self.collections = self.db.list_collection_names()
-        self.guangdong = self.db['广东']
-        result = self.guangdong.aggregate(
-            [
-                {"$group":
+        for collection in self.collections:
+            self.parse_data(collection)
+
+    def parse_data(self, city_collection):
+        collection = self.db[city_collection]
+        data = collection.find_one(
+            {
+                '$and':[
                     {
-                        "_id":
-                            {
-                                "_id": "$_id",
-                                # "city": "$city",
-                                # "xiaoqu_name": "$xiaoqu_name",
-                                # "position": "$position"
-                            },
-                        "count": {
-                            "$avg": 1
+                        'main_price':{
+                            '$gte':1000,
+                            '$lt':10000
                         }
                     }
-                }
-            ]
+                ]
+            }
         )
-        for i in result:
-            print(i)
-        # for collection in self.collections:
-
-    # 总价最贵房子top10
-    def total_price_house_max_top10(self):
-        data = []
-
-    # 总价最低房子top10
-    def total_price_house_min_top10(self):
-        data = []
-
-    # 每平米最贵房子top10
-    def unit_price_max_house_top10(self):
-        data = []
-
-    # 每平米最便宜房子top10
-    def unit_price_min_house_top10(self):
-        data = []
-
-    # 总价最贵小区top10
-    def total_price_max_xiaoqu_top10(self):
-        data = []
-
-    # 总价最低小区top10
-    def total_price_min_xiaoqu_top10(self):
-        data = []
-
-    # 每平米最贵小区top10
-    def unit_price_max_xiaoqu_top10(self):
-        data = []
-
-    # 每平米最便宜小区top10
-    def unit_price_min_xiaoqu_top10(self):
-        data = []
-
-    # 每套房均价
-    def total_price_house_average(self):
-        data = []
-
-    # 每平米均价
-    def unit_price_house_average(self):
-        data = []
-
-    # 每个小区每套均价
-    def total_price_xiaoqu_range(self):
-        data = []
-
-    # 每个小区每平米均价
-    def unit_price_xiaoqu_range(self):
-        data = []
-
+        print(data)
+        exit(0)
 
 if __name__ == '__main__':
-    db = 'lianjia20190421'
-    mongo = mongo_dao(db)
+    mongo = mongo_dao()
